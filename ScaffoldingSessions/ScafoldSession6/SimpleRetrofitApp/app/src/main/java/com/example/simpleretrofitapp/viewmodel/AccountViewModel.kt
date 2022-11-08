c93d3b7d228015542591b7440f2077f53556d69f
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-11-08 15:33:04 +0300

    updated

--- ScaffoldingSessions/ScafoldSession6/SimpleRetrofitApp/app/src/main/java/com/example/simpleretrofitapp/viewmodel/AccountViewModel.kt
+++ ScaffoldingSessions/ScafoldSession6/SimpleRetrofitApp/app/src/main/java/com/example/simpleretrofitapp/viewmodel/AccountViewModel.kt
@@ -3,7 +3,6 @@ package com.example.simpleretrofitapp.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.simpleretrofitapp.data.model.Account
-import com.example.simpleretrofitapp.data.remote.repo.AccountRepo
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
