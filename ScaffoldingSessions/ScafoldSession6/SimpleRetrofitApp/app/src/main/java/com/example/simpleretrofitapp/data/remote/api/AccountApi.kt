b75bb60ed09244aee6e0689238325dc2f854aa3e
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-11-08 16:08:53 +0300

    Update AccountApi.kt

--- ScaffoldingSessions/ScafoldSession6/SimpleRetrofitApp/app/src/main/java/com/example/simpleretrofitapp/data/remote/api/AccountApi.kt
+++ ScaffoldingSessions/ScafoldSession6/SimpleRetrofitApp/app/src/main/java/com/example/simpleretrofitapp/data/remote/api/AccountApi.kt
@@ -1,8 +1,12 @@
 package com.example.simpleretrofitapp.data.remote.api
 
 import android.accounts.Account
+import retrofit2.http.GET
 
+
+//https://gist.githubusercontent.com/abdalabaaji/31714e28bc6ed0b506c90e5ed00f0f59/raw/ba04331fdbb59ef0af192f8362c568b4dfe5c7b3/users
 interface AccountApi {
+    @GET("")
     fun getAccounts() : List<Account>
 
 }
