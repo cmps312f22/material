f6deb76ce6bb174b083094f4a971f40d0e4da688
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-11-09 14:43:53 +0300

    updated

--- ScaffoldingSessions/ScafoldSession6/SimpleRetrofitApp/app/src/main/java/com/example/simpleretrofitapp/data/remote/api/AccountApi.kt
+++ ScaffoldingSessions/ScafoldSession6/SimpleRetrofitApp/app/src/main/java/com/example/simpleretrofitapp/data/remote/api/AccountApi.kt
@@ -1,4 +1,8 @@
 package com.example.simpleretrofitapp.data.remote.api
 
+
+
+//https://gist.githubusercontent.com/abdalabaaji/31714e28bc6ed0b506c90e5ed00f0f59/raw/db27d337b5d5d5e0a050cb1b12d5686a592ab2d6/accounts
 interface AccountApi {
+
 }
--- ScaffoldingSessions/ScafoldSession6/SimpleRetrofitApp/app/src/main/java/com/example/simpleretrofitapp/data/remote/repo/AccountRepo.kt
+++ ScaffoldingSessions/ScafoldSession6/SimpleRetrofitApp/app/src/main/java/com/example/simpleretrofitapp/data/remote/repo/AccountRepo.kt
@@ -1,4 +1,5 @@
 package com.example.simpleretrofitapp.data.remote.repo
 
 object AccountRepo {
+
 }
