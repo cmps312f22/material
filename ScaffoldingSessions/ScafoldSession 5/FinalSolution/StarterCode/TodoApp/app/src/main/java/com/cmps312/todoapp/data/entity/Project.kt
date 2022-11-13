3b5d00e2d210fb0bd02fbe6e45786cdfbbb0471b
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-11-13 17:07:16 +0300

    updated

--- ScaffoldingSessions/ScafoldSession 5/FinalSolution/StarterCode/TodoApp/app/src/main/java/com/cmps312/todoapp/data/entity/Project.kt
+++
@@ -1,9 +0,0 @@
-package com.cmps312.todoapp.data.entity
-
-import com.google.firebase.firestore.DocumentId
-
-data class Project(
-    //todo make it an document id
-    var id: String = "",
-    var name: String? = "",
-)
--- ScaffoldingSessions/ScafoldSession6/SolutionWorking/SimpleRetrofitApp/app/src/main/java/com/example/simpleretrofitapp/data/remote/repo/AccountRepo.kt
+++ ScaffoldingSessions/ScafoldSession6/SolutionWorking/SimpleRetrofitApp/app/src/main/java/com/example/simpleretrofitapp/data/remote/repo/AccountRepo.kt
@@ -1,12 +1,16 @@
 package com.example.simpleretrofitapp.data.remote.repo
 
+import android.util.Log
+import com.example.simpleretrofitapp.data.model.Account
 import com.example.simpleretrofitapp.data.remote.api.AccountApi
+import kotlinx.coroutines.runBlocking
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object AccountRepo {
     val BASE_URL =
         "https://gist.githubusercontent.com/abdalabaaji/31714e28bc6ed0b506c90e5ed00f0f59/raw/db27d337b5d5d5e0a050cb1b12d5686a592ab2d6/"
+
     // TODO 3: Create a Retrofit object
     val accountApi by lazy {
         Retrofit.Builder()
@@ -17,6 +21,19 @@ object AccountRepo {
     }
 
     suspend fun getAccounts() = accountApi.getAccounts()
+
+    //TODO 4: Create a function to add the data to firebase
+    //fun addAccounts(account : Account) = db.collection("accounts").add(account)
+    init {
+        initializeAccounts()
+    }
+
+    private fun initializeAccounts() {
+        val data = runBlocking { getAccounts() }
+        Log.d("The data TAG", "initializeAccounts: $data")
+        //TODO 4: Add data to firebase
+        //        data.forEach { addAccounts(it) }
+    }
 }
 //
 ////import okhttp3.MediaType.Companion.toMediaType
